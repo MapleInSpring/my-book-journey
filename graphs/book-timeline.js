c203142c3bb7083880a8a8498f9828a428baabdf
MapleInSpring <hyf94244280@gmail.com>
2017-09-19 14:33:34 +0800

    Make read time into circles

--- graphs/book-timeline.js
+++ graphs/book-timeline.js
@@ -1,13 +1,19 @@
+let width = 500, height = 100;
 let body = d3.select('body'), books;
+let svg = body.append('svg')
+    .attr('width', width)
+    .attr('height', height);
 
 const addBooks = () => {
     d3.json("books.json", (bs) => {
         books = bs;
-        body.selectAll('div')
+        svg.selectAll('circle')
             .data(books)
             .enter()
-            .append('div')
-            .attr('class', "bar")
-            .style('width', d => d.days * 5 + 'px');
+            .append('circle')
+            .classed('circleDays', true)
+            .attr('cx', (_, i) => i * 75 + 25)
+            .attr('cy', height / 2)
+            .attr('r', (d) => d.days);
     });
 };
